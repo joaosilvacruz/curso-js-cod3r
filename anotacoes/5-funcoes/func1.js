8721809b1e449827af87c1f6d5e9dbb7b62df2a7
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-02-15 12:40:42 -0300

    criado arquivos

---
+++ anotacoes/5-funcoes/func1.js
@@ -0,0 +1,10 @@
+// A função no javascript é tida como um dado qualquer, por isso é possível:
+
+// Criar uma função de forma literal
+function fun1() {}
+
+// Armazenar uma função em uma variável
+const func2 = function() {}
+
+// Armazenar uma função dentro de um array
+const array
