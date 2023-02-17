4d31051cfd082b3e017e8ec21784be1d6690f0ad
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-02-17 12:18:58 -0300

    adicionado closure

--- anotacoes/5-funcoes/closure.js
+++ anotacoes/5-funcoes/closure.js
@@ -1,2 +1,17 @@
 // Closure é o escopo criado quando uma funçãoé declarada
 // Esse escopo permite a função acessar e manipular variáveis externas à função
+
+// Contexto léxico
+
+const x = 'Global'
+
+function fora() {
+    const x = 'Local'
+    function dentro() {
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
