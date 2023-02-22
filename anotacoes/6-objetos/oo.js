8a0ddbf53600454710ab43367189e4e925bccce0
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-02-22 15:15:33 -0300

    adicionado anotacoes

---
+++ anotacoes/6-objetos/oo.js
@@ -0,0 +1,16 @@
+// o código abaixo é não executável
+
+// Paradigma procedural
+processamento(valor1, valor2, valor3)
+
+// Orientação a objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento()
