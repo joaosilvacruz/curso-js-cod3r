b9def9d76bcce8d0737077743cc5174efcb69e70
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-02-27 12:28:59 -0300

    Revert "adicionado anotacoes"
    This reverts commit c9e8064b08352e49d41d5959f4cccee1ceef7945.

--- anotacoes_node/moduloA.js
+++ anotacoes_node/moduloA.js
@@ -1,5 +1,3 @@
-// Formas para exportar no node
-
 this.ola = 'Fala pessoal'
 exports.bemVindo = 'Bem vindo ao node!'
 module.exports.ateLogo = 'Até o próximo exemplo'
--- anotacoes_node/moduloB.js
+++ anotacoes_node/moduloB.js
@@ -1,5 +1,3 @@
-// Forma de exportar modulos no node
-
 module.exports = {
     bomDia: 'Bom dia',
     boaNoite() {
