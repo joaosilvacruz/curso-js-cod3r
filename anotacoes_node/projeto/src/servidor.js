58602c4539b72046fc6bb0307a8f463bc70c10ac
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-03-01 14:42:39 -0300

    alteracoes servidor

--- anotacoes_node/projeto/src/servidor.js
+++ anotacoes_node/projeto/src/servidor.js
@@ -1,13 +1,30 @@
-const porta = 3004
+const porta = 3001
 
 const express = require('express')
 const app = express()
 const bancoDeDados = require('./bancoDeDados')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos()) // .send() converte para JSON 
-    
 })
+
+// Criando outro get para repassar valores de acordo com id
+// O :id significa usar o id correspondente (1, 2, 3, 4)
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
 })
